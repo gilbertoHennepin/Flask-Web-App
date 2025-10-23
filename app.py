e987646dbcd336fa93d93adc33ac60dc1879034b
Gilberto Ojeda <email>
2025-10-22 21:34:34 -0500

    edit

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request  # NOT the same as requests 
+from github_api import get_github_user_info
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # home page
 def homepage():
     return render_template('index.html')
+
+@app.route('/get_user')
+def get_user():
+    #get user info from github api and displaay on new page
+    print('form data is', request.args)  # for GET requests, use request.args is a dictionary
+    username = request.args.get('username') # safer, retursn none if no username
+
+    user_info, error_message = get_github_user_info(username) # dictionary of user info
+    if error_message:
+        return render_template('error.html', error_message=error_message)
+    else:
+        return render_template('github.html', user=user_info)
+
+    return render_template('github.html', user)
 
 
 if __name__ == '__main__':
--- github_api.py
+++ github_api.py
@@ -1,1 +1,44 @@
 # TODO make requests to github API
+
+import requests
+import logging
+
+def get_github_user_info(username):
+
+
+    # 404 - username not found
+    # other errors 
+    # success 
+
+    # retrurn a tuple of (data, error)
+    # if things work, return (data, None)
+
+    # if things font work theres an error, return (None, error_message)
+
+    try:
+
+        response = requests.get(f'https://api.github.com/users/{username}')
+        if response.status_code == 404:
+            return None, f'Username {username} not found'
+        response.raise_for_status()
+    # todo error handling
+        response_json = response.json()
+        user_info = extract_user_info(response_json)
+        return user_info, None
+    except Exception as e:
+        logging.exception(e)
+        return None, 'Error connecting to GitHub API'
+
+def extract_user_data(json_response):
+    # TODO extract useful info from github_response
+    return {
+        'login': json_response.get('login'),
+        'name': json_response.get('name'),
+        'avatar_url': json_response.get('avatar_url'),
+        'home_page': json_response.get('html_url'),
+        'repos': json_response.get('public_repos'),
+        'bio': json_response.get('bio'),
+    }
+
+
+
